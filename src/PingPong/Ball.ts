8030b8b8b197678891fe62d16afddfb2c62b6420
Hatsu <inciesso@gmail.com>
2019-11-07 18:34:11 +0800

    We have a ball that moves and bounce (no acceleration) now

--- src/PingPong/Ball.ts
+++ src/PingPong/Ball.ts
@@ -1,4 +1,14 @@
 class Ball extends eui.Component implements eui.UIComponent {
+
+	// Velocity
+	protected v_x: number = 0;
+	protected v_y: number = 0;
+	// Acceleration
+	protected a_x: number = 0;
+	protected a_y: number = 0;
+	// Radius
+	protected radius: number = 10;
+
 	public constructor() {
 		super();
 	}
@@ -18,26 +28,65 @@ class Ball extends eui.Component implements eui.UIComponent {
 	}
 
 
+	public setVelocity(x: number, y: number):Ball
+	{
+		this.v_x = x;
+		this.v_y = y;
+		return this;
+	}
+
+	public setAcceleration(x: number, y: number):Ball
+	{
+		this.a_x = x;
+		this.a_y = y;
+		return this;
+	}
+
+
 	protected init():void // 業務邏輯
 	{
 		this.addEventListener(
 			egret.TouchEvent.ENTER_FRAME,
 			this.doEnterFrame,
 			this
-		)
+		);
 	}
 
 
 	protected doEnterFrame():void
 	{
 		// 暫時找不到類似 C# Action 或 Event 的功能，先這樣。要改 ENTER_FRAME 做了啥事就改這邊。
-		this.move();
+		// this.move();
 	}
 
 
-	protected move():void
+	public move():void
 	{
-		this.x++;
+		this.x += this.v_x;
+		this.y += this.v_y;
+
+		if (this.x + this.radius >= this.parent.width)
+		{
+			this.x = this.parent.width - this.radius;
+			this.v_x *= -1;
+		}
+		else if (this.x - this.radius <= 0)
+		{
+			this.x = 0 + this.radius;
+			this.v_x *= -1;
+		}
+		if (this.y + this.radius >= this.parent.height)
+		{
+			this.y = this.parent.height - this.radius;
+			this.v_y *= -1;
+		}
+		else if (this.y - this.radius <= 0)
+		{
+			this.y = 0 + this.radius;
+			this.v_y *= -1;
+		}
+		
+		// this.v_x += this.a_x;
+		// this.v_y += this.a_y;
 	}
-	
 }
--- src/PingPong/PingPong.ts
+++ src/PingPong/PingPong.ts
@@ -1,4 +1,7 @@
 class PingPong extends eui.Component implements  eui.UIComponent {
+
+	protected ball: Ball;
+
 	public constructor() {
 		super();
 	}
@@ -12,6 +15,32 @@ class PingPong extends eui.Component implements  eui.UIComponent {
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
+
+		this.init();
+	}
+
+
+	protected init():void
+	{
+		this.addEventListener(
+			egret.TouchEvent.ENTER_FRAME,
+			this.doEnterFrame,
+			this
+		);
+		this.ball.setVelocity(5, 5);
+	}
+
+
+	protected doEnterFrame():void
+	{
+		// 暫時找不到類似 C# Action 或 Event 的功能，先這樣。要改 ENTER_FRAME 做了啥事就改這邊。
+		this.running();
+	}
+
+
+	protected running():void // 一直跑一直跑
+	{
+		this.ball.move();
 	}
 	
 }
