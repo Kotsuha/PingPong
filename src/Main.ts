a8386230146cfc66f6598cd5bb34edd8e7d2df09
Hatsu <inciesso@gmail.com>
2019-11-07 16:23:11 +0800

    Main add ball

--- src/Main.ts
+++ src/Main.ts
@@ -156,6 +156,13 @@ class Main extends eui.UILayer {
         button.verticalCenter = 0;
         this.addChild(button);
         button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+
+        // ---------------------------------------------------------------------
+
+        let ball = new Ball();
+        this.addChild(ball);
+        ball.x = 200;
+        ball.y = 200;
     }
     /**
      * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
